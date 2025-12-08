6d56082afbac70c890e02976e14221fbae040a54
Be2monkey <D303894@edu.curio.nl>
2025-12-08 11:25:40 +0100

    gebruikers aanmaken/verwijderen
    als admin kan je nu naar de gebruikers pagina waar je alle gebruikers kan vinden en je kan nieuwe aanmaken en oude gebruikers aanpassen of verwijderen

--- routes/web.php
+++ routes/web.php
@@ -158,6 +158,8 @@ Route::middleware('auth')->group(function () {
     // Admin routes
 
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
     // User management actions
     Route::post('/users/{user}/role', [UserController::class, 'updateRole'])->name('users.update.role');
